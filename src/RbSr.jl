32984488b3e0fd434ad31fff0f63e2b56e0523aa
Jarred C Lloyd <jarredclloyd@gmail.com>
2023-01-16 20:58:08 +1030

    Update RbSr.jl

--- src/RbSr.jl
+++ src/RbSr.jl
@@ -6,58 +6,67 @@ export ageRbSr, RbSrAgeNorm, RbSrAgeInv
 
 #Rb-Sr
 
-function ageRbSr(β₁::Real, β₁SE::Real=0, β₀::Real=0, β₀SE::Real=0, σᵦ₁ᵦ₀::Real=0; inverse = false, SElevel_in = 2, 
+function ageRbSr(β₁, β₀=0.0, β₁SE=0.0, β₀SE=0.0, σᵦ₁ᵦ₀=0.0; inverse = false, SElevel_in = 2, 
     SElevel_out = 2)
     if inverse == false
         if isa(β₁, Array) == true
-        age = zeros(size(β1))
-        ageSE = zeros(size(β1SE))
-        nslopes = length(β₁)
-        @simd for i in 1:nslopes
-            age[i], ageSE[i] * SElevel_out = RbSrAgeNorm(β₁[i], β₁SE[i] / SElevel_in)
+            nslopes = length(β₁)
+            age = zeros(nslopes)
+            ageSE = zeros(nslopes)
+            if isa(β₁SE, Array) == false
+                β₁SE = zeros(nslopes)
+            end
+            @simd for i in 1:nslopes
+                age[i], ageSE[i] = RbSrAgeNorm(β₁[i], β₁SE[i] / SElevel_in)
+                ageSE[i] = ageSE[i] * SElevel_out
             end
         else 
-            age, ageSE * SElevel_out = RbSrAgeNorm(β₁[i], β₁SE[i] / SElevel_in)
+            age, ageSE = RbSrAgeNorm(β₁, β₁SE / SElevel_in)
+            ageSE = ageSE * SElevel_out
         end
     elseif inverse == true
-        if isa.(β₀, Real) !== true || if isa.(β₁, Real) !== true
-            error("Both the intercept (β₀) and the slope (β₁) are required for inverse isochron age calculations.")
+        if .<=(β₀, 0.0) == true || .<=(β₁, 0.0) == true
+            error("Real values (>0) are required for both the intercept (β₀) and the slope (β₁) to calculate ages for 
+            inverse isochron data.")
         end
         if isa(β₁, Array) == true && isa(β₀, Array) == true && length(β₁) == length(β₀)
-            age = zeros(size(β1))
-            ageSE = zeros(size(β1SE))
             nslopes = length(β₁)
+            age = zeros(nslopes)
+            ageSE = zeros(nslopes)
+            if isa(β₀SE, Array) == false
+                β₀SE = zeros(nslopes)
+            end
+            if isa(β₁SE, Array) == false
+                β₁SE = zeros(nslopes)
+            end
+            if isa(σᵦ₁ᵦ₀, Array) == false
+                σᵦ₁ᵦ₀ = zeros(nslopes)
+            end
             @simd for i in 1:nslopes
-                age[i], ageSE[i] * SElevel_out = RbSrAgeInv(β₀[i], β₁[i], β₀SE[i] / SElevel_in, β₁SE[i] / SElevel_in, σᵦ₁ᵦ₀[i])
-                end
+                age[i], ageSE[i] = RbSrAgeInv(β₀[i], β₁[i], β₀SE[i] / SElevel_in, β₁SE[i] / SElevel_in, σᵦ₁ᵦ₀[i])
+                ageSE[i] = ageSE[i] * SElevel_out
+            end
         else 
-            age, ageSE * SElevel_out = RbSrAgeInv(β₀[i], β₁[i], β₀SE[i] / SElevel_in, β₁SE[i] / SElevel_in)
+            age, ageSE = RbSrAgeInv(β₀, β₁, β₀SE / SElevel_in, β₁SE / SElevel_in)
+            ageSE = ageSE * SElevel_out
         end
     end
     return age, ageSE
-end
+end 
 
-function RbSrAgeNorm(β₁::Real, β₁SE::Real=0)
+function RbSrAgeNorm(β₁::Real, β₁SE::Real=0.0)
     date = log(β₁ + 1) / λRb87
     dateSE = abs(log(β₁SE + 1) / λRb87)
-    if dateSE > 0
-        return date, dateSE
-    else
-        return date
-    end
+    return date, dateSE
 end
 
-function RbSrAgeInv(β₀::Real, β₁::Real, β₀SE::Real=0, β₁SE::Real=0, σᵦ₁ᵦ₀::Real=0)
-    if isa(β₀, Real) !== true || if isa(β₁, Real) !== true
+function RbSrAgeInv(β₀::Real, β₁::Real, β₀SE::Real=0.0, β₁SE::Real=0.0, σᵦ₁ᵦ₀::Real=0.0)
+    if isa(β₀, Real) !== true || isa(β₁, Real) !== true
         error("Both the intercept (β₀) and the slope (β₁) are required for inverse isochron age calculations.")
     end
     ratio = inv(-β₀ / β₁)
     date = log(ratio + 1) / λRb87
     ratioSE = ratio * sqrt((β₀SE / β₀)^2 + (β₁SE / β₁)^2 - 2 * σᵦ₁ᵦ₀ / (β₀ * β₁))
     dateSE = abs(log(ratioSE + (β₀ ^2 * β₀SE ^2 + β₁ ^2 * β₁SE ^2 + 2 * β₀ * β₁ * σᵦ₁ᵦ₀) + 1) / λRb87)
-    if dateSE > 0
-        return date, dateSE
-    else
-        return date
-    end
+    return date, dateSE
 end
