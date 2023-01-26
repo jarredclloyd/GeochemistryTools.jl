125f2bc05e2f7664543cc52f6c0f4abcb0229cc8
Jarred C Lloyd <jarredclloyd@gmail.com>
2023-01-26 11:49:15 +1030

    Update Profilometer.jl

--- src/Profilometer.jl
+++ src/Profilometer.jl
@@ -5,15 +5,16 @@ Functions for laser profilometry (Olympus OLS5000)
 export loadProfilometer
 
 """
-    loadProfilometer(hostdir::string, sample::string; pixelwidth::Int=1024, headerrow::Int=19, datarow::Int=20)
+    loadProfilometer(hostdir::string, sample::string; headerrow::Int=19, datarow::Int=20)
 
     Loads Olympus OLS5000 profilometer CSV data (exported as area)
 """
 function loadProfilometer(hostdir, sample; headerrow::Int=19, datarow::Int=20)
     file = glob(sample * "*.csv", hostdir)
-    df = CSV.read(file, DataFrame; header=false, skipto=5, limit=2, select=[1, 2])
-    yresolution = df[1, 2]
-    zresolution = df[2, 2]
+    df = CSV.read(file, DataFrame; header=false, skipto=4, limit=3, select=[1, 2])
+    xresolution = df[1, 2]
+    yresolution = df[2, 2]
+    zresolution = df[3, 2]
     df = CSV.read(file, DataFrame; header=headerrow, skipto=datarow, select=(index, name) -> occursin("POS", 
         String(name)) == true || occursin("DataLine", String(name)) == true)
     pixelwidth = ncol(df)
@@ -22,6 +23,8 @@ function loadProfilometer(hostdir, sample; headerrow::Int=19, datarow::Int=20)
     df.y = 0 .+ parse.(Int, df.POS) .* yresolution
     select!(df, :DataLine => :x, :y, :value => :z)
     df.z = (df[!, :z]) .- mode(df[!, :z])
-    metadata!(df, "xyresolution", yresolution); metadata!(df, "zresolution", zresolution);
+    metadata!(df, "xresolution", xresolution);
+    metadata!(df, "yresolution", yresolution);
+    metadata!(df, "zresolution", zresolution);
     return df
 end
