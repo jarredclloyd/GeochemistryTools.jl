1c9f872158c81d749313b630c84ac07d1e0aad71
Jarred C Lloyd <jarredclloyd@gmail.com>
2023-06-23 22:54:25 +0930

    Update ICP_MS.jl
    Update documentation, rename load_downhole

--- src/ICP_MS.jl
+++ src/ICP_MS.jl
@@ -2,105 +2,73 @@
 This file contains tools for (Agilent) ICP-MS data
 =#
 
-export loadDownhole, plot_dh_scatter
+export load_downhole, plot_dh_scatter
 
 """
-    loadDownhole(hostdir, sample, CPS_col1, CPS_col2, [firstrow, stabletime, signalend, trim])
+    load_downhole(args...; kwargs...)
 
 Load and prepare data from CSV agilent CSV files to assess downhole fractionation.
 
-# Description
-Load all relevant files in the hostdir using glob (e.g. NIST610 will just load `NIST610*.csv` files, whereas NIST 
-will load any file where the name contains `NIST*.csv`). 
+Load all relevant files in the host_directory using glob, concatenates the data into one table, calculates ratio between
+`cps_column1` and `cps_column2`, the median-normalised ratio, and sorts the table by time.
 
-`CPS_cols` should be specified as element mass (e.g., Rb85). It matches using regular expression and normalises the 
-header names. If duplicate isotope values are present (due to mass shift checks, e.g. K39 -> 39, K39 -> 58) specify the 
-desired mass by replacing ` -> ` with `_` (e.g. `K39 -> 39` to `K39_39`).
+# Arguments
+- `host_directory::AbstractString`: The folder path containing raw LA-ICP-MS CSV files.
+- `sample::AbstractString`: The sample name as a string.
+- `cps_column1::AbstractString`: A string representing the first desired column with counts per second data.
+- `cps_column2::AbstractString`: A string representing the second desired column with counts per second data.
 
-Concatenates the data into one table, calculates ratio between `CPS_col1` and `CPS_col2`, the median-normalised ratio, 
-and sorts the table by time. 
+## Argument Notes
+- `host_directory` will need to be wrapped by `raw` on Windows operating systems.
+-  `sample` only needs to be a unique portion of the sample name. E.g. NIST610 will just load `NIST610*.csv` files, whereas NIST will load any file where the name contains `NIST*.csv`).
+- `cps_columnX` should be specified as `element` then `isotopic mass` (e.g., `Rb85`). It matches using regular expression and normalises the header names. If duplicate isotope values are present (due to mass shift checks, e.g. K39 -> 39, K39 -> 58) specify the desired mass by replacing ` -> ` with `_` (e.g. `K39 -> 39` to `K39_39`).
 
-It is recommended to specify the `stabletime` parameter to the point where the signal counts are stabilised. It is set by 
-default to be 32 (i.e. 32 seconds). This parameter is used to filter the rows to calculate the median CPS ratio. If you 
-want to remove the rows prior to the stable signal time, adjust `firstrow` to equal the row equivalent to the stabletime
-(usually about 110 for a 30 second gas blank).
-
-By default `signalend` is set to `Inf`, as such it will use the entire signal. It is recommended to set this to a value
-appropriate to your data (i.e. total signal time minus ~5 [e.g. 65]) to avoid some artefacts that may occur near the end 
-of an ablation.
-
-By default `trim` is set to `false`. If set to `true` it will delete all data from the first record past `signalend` to 
-the last record.
+# Keywords
+- `firstrow::Int`: An `Int` representing the first data row. Default value is `5`. This can be used to remove the rows prior to `stabletime` if set to an appropriate value (e.g, ~110 for a 30 second gas blank).
+- `stabletime::Real`: The time (in seconds) where signal counts are stabilised. Default value is `32` (i.e. 32 seconds). This parameter is used to filter the rows when calculating the median counts per second ratio.
+- `signalend::Real`: The time (in seconds) where the data should be truncated. Default value is `Inf` (will use entire signal beyond `stabletime`). It is recommended to set this to a value appropriate to your data (i.e. total signal time minus ~5 [e.g. 65]) to avoid some artefacts  that may occur near the end of an ablation.
+- `trim::Bool`: Whether to trim the data to only contain values between `firstrow` and `signalend`. Default value is `false`. Setting to `true` it will delete all data from the first record past `signalend` to the last record.
 
 # Example
 ```
-julia> loadDownhole("path/to/dir", "sample_x", "Rb85", "Sr87"; firstrow = 5, stabletime = 110)
+julia> load_downhole("path/to/dir", "sample_x", "Rb85", "Sr87"; firstrow = 110, stabletime = 32, signalend = 65, trim = true)
 MxN DataFrame
 
 ```
 """
-function loadDownhole(hostdir, sample::String, CPS_col1::String, CPS_col2::String;
-    firstrow::Int = 5, stabletime::Number = 32, signalend::Number = Inf, trim = false)
-    files = glob(sample * "*.csv", hostdir)
-    data = CSV.read(files, DataFrame; header = 4, skipto = firstrow, types = Float64, footerskip = 3, 
-        ignoreemptyrows = true, normalizenames = true)
-    data = select!(data, r"Time", r"" * CPS_col1, r"" * CPS_col2)
-    rename!(data, ["time", CPS_col1, CPS_col2])
+function load_downhole(
+    host_directory::AbstractString,
+    sample::AbstractString,
+    cps_column1::AbstractString,
+    cps_column2::AbstractString;
+    firstrow::Int = 5,
+    stabletime::Real = 32,
+    signalend::Real = Inf,
+    trim::Bool = false,
+)
+    files = glob(sample * "*.csv", host_directory)
+    data = CSV.read(
+        files,
+        DataFrame;
+        header = 4,
+        skipto = firstrow,
+        types = Float64,
+        footerskip = 3,
+        ignoreemptyrows = true,
+        normalizenames = true,
+    )
+    data = select!(data, r"Time", r"" * cps_column1, r"" * cps_column2)
+    rename!(data, ["time", cps_column1, cps_column2])
     sort!(data, :time)
-    data.ratio =  data[!, CPS_col1] ./ data[!, CPS_col2]
+    data.ratio = data[!, cps_column1] ./ data[!, cps_column2]
     replace!(data[!, :ratio], Inf => 0)
     mdn = median(filter(x -> x .> 0, data[stabletime .< data.time .< signalend, :ratio]))
-    data.ratio_mdn_norm = data[!,:ratio] ./ mdn
+    data.ratio_mdn_norm = data[!, :ratio] ./ mdn
     if trim == true
         filter!(:time => x -> x .< signalend, data)
     end
-    metadata!(data, "name", sample); metadata!(data,"stable time", stabletime); metadata!(data,"signal end", signalend)
+    metadata!(data, "name", sample)
+    metadata!(data, "stable time", stabletime)
+    metadata!(data, "signal end", signalend)
     return data
-end
-
-"""
-    plot_dh_scatter(DataFrame; [stable_time, laser_start, normalised, marker_size, marker_colour, line_colour])
-
-# Description
-Create a basic scatter plot of the downhole fractionation pattern for a dataset loaded via `load_downhole`.
-
-Can specify `name`, `stable_time`, `laser_start`, `normalised`, `marker_size`, `marker_colour`, and `line_colour`. 
-Only requires the DataFrame name, and will default to the median-normalised plot. 
-Name and `stable_time` will be taken from the metadata of the input DataFrame if loaded using `load_downhole()`. 
-`laser_start` will default to `stable_time - 2` but can be adjusted to whatever value you want the plot to start at.
-
-# Example
-```
-julia>  plot_dh_scatter(df)
-
-```
-"""
-function plot_dh_scatter(data::DataFrame; 
-    name = metadata(data, "name"),
-    stable_time = metadata(data, "stable_time"),
-    laser_start = stable_time - 2,
-    normalised::Bool = true,
-    marker_size = 5,
-    marker_colour = "#800080", 
-    line_colour = :orange)
-    fig1 = Figure(resolution=(1920,1080))
-    if normalised == true
-        fit = Polynomials.fit(data[data.time .> stable_time, :time], data[data.time .> stable_time, :ratio_mdn_norm], 3)
-        ax1 = Axis(fig1[1, 1]; title = "Median normalised downhole fractionation — " * name, xlabel = "Time (s)",
-            ylabel = "(CPS/CPS)/median")
-        scatter!(ax1, data[data.time .> laser_start, :time], data[data.time .> laser_start, :ratio_mdn_norm],
-         color = marker_colour, markersize = marker_size)
-        lines!(ax1, data[data.time .> stable_time, :time], coeffs(fit)[1] .+ coeffs(fit)[2] .* 
-            data[data.time .> stable_time, :time] .+ coeffs(fit)[3] .* data[data.time .> stable_time, :time] .^ 2 .+
-            coeffs(fit)[4] .* data[data.time .> stable_time, :time] .^ 3, color = line_colour)
-    elseif normalised == false
-        fit = Polynomials.fit(data[data.time .> stable_time, :time], data[data.time .> stable_time, :ratio], 3)
-        ax1 = Axis(fig1[1, 1]; title = "Downhole fractionation — " * name, xlabel ="Time (s)", ylabel="CPS/CPS")
-        scatter(ax1, data[data.time .> laser_start, :time], data[data.time .> laser_start, :ratio],
-         color = marker_colour, markersize = marker_size)
-        lines!(ax1, data[data.time .> stable_time, :time], coeffs(fit)[1] .+ coeffs(fit)[2] .* 
-            data[data.time .> stable_time, :time] .+ coeffs(fit)[3] .* data[data.time .> stable_time, :time] .^ 2 .+
-            coeffs(fit)[4] .* data[data.time .> stable_time, :time] .^ 3, color = line_colour)
-    end
-    return fig1
 end
