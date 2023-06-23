93d9c7a5cc5a4b901c40ca60389670ab2ad910ee
Jarred C Lloyd <jarredclloyd@gmail.com>
2023-06-23 22:53:56 +0930

    Update EIVLinearRegression.jl
    Update dictionary pointers, formatting

--- src/EIVLinearRegression.jl
+++ src/EIVLinearRegression.jl
@@ -27,12 +27,14 @@ Compute line of best fit using York errors-in-variables regression algorithm.
 
 Input df as a DataFrame of 4 of 5 columns wide with column order (X, sX, Y, sY, [ρXY]).
 
-# Optional arguments 
+# Optional arguments
 SElevel: standard error level, input as an integer. \n
 SEtype: standard error type as a string of value "abs" OR "rel". \n
-SrInitial: ⁸⁷Sr/⁸⁶Srᵢ value. Can be input as a string key from `Dict_SrInitial`, as a single numeric value, or as a vector of the initial and its standard error (same SElevel as input data). E.g. SrInitial = "MDCInv", SrInitial = 0.72, OR SrInitial = [0.72, 0.01].
+SrInitial: ⁸⁷Sr/⁸⁶Srᵢ value. Can be input as a string key from `dict_sr87_sr86i`, as a single numeric value, or as a
+vector of the initial and its standard error (same SElevel as input data). E.g. SrInitial = "MDCInv", SrInitial = 0.72,
+OR SrInitial = [0.72, 0.01].
 
-For a full list of available keys in `Dict_SrInitial`, type `keys(Dict_SrInitial)`
+For a full list of available keys in `dict_sr87_sr86i`, type `keys(dict_sr87_sr86i)`
 
 # Example
 ```julia-repl
@@ -43,13 +45,13 @@ julia> york(df, 2, "abs"; SrInitial = "MDCInv")
 # Reference
 York et al. 2004 doi:https://doi.org/10.1119/1.1632486.
 """
-function yorkfit(df::DataFrame; SElevel::Int=2, SEtype::String="abs", SrInitial::Any=nothing)
+function yorkfit(df::DataFrame; SElevel::Int = 2, SEtype::String = "abs", SrInitial::Any = nothing)
     dfCols::Int64 = ncol(df)
     dfRows = nrow(df)
     if SrInitial !== nothing
-        if isa(SrInitial, String) == true && haskey(Dict_SrInitial, SrInitial) == true
-            SrInitial = Dict_SrInitial[SrInitial]
-        elseif length(SrInitial) == 1 && isa(SrInitial, Number)    
+        if isa(SrInitial, String) == true && haskey(dict_sr87_sr86i, SrInitial) == true
+            SrInitial = dict_sr87_sr86i[SrInitial]
+        elseif length(SrInitial) == 1 && isa(SrInitial, Number)
             SrInitial = [1e-10, 1e-18, SrInitial, 0.01]
         elseif length(SrInitial) == 2 && isa(SrInitial[1], Number) && isa(SrInitial[2], Number)
             SrInitial = [1e-10, 1e-18, SrInitial[1], SrInitial[2]]
@@ -57,13 +59,13 @@ function yorkfit(df::DataFrame; SElevel::Int=2, SEtype::String="abs", SrInitial:
             throw(ArgumentError("Only the initial Sr ratio value and its uncertainty is required."))
         else
             throw(ArgumentError("""
-            SrInitial variable is malformed. 
-            Use either a string that matches a fixed value or reference material name: 
-                e.g. "MDC", "MDCinv", or "SolarSystem" 
-            Or input a single numeric value or two-length numeric vector: 
-                e.g. 0.72 or [0.7200, 0.0010] 
-             
-            For a full list of available reference material keys: keys(Dict_SrInitial)
+            SrInitial variable is malformed.
+            Use either a string that matches a fixed value or reference material name:
+                e.g. "MDC", "MDCinv", or "SolarSystem"
+            Or input a single numeric value or two-length numeric vector:
+                e.g. 0.72 or [0.7200, 0.0010]
+
+            For a full list of available reference material keys: keys(dict_sr87_sr86i)
             """))
         end
         if dfCols == 4
@@ -74,28 +76,33 @@ function yorkfit(df::DataFrame; SElevel::Int=2, SEtype::String="abs", SrInitial:
             push!(df, temp)
         end
         # elseif length(SrInitial) == 1 && isa(SrInitial, Number)
-            # push!(df, [1e-10, 1e-18, SrInitial, 0.01])
+        # push!(df, [1e-10, 1e-18, SrInitial, 0.01])
         # elseif length(SrInitial) == 2 && isa(SrInitial[1], Number) && isa(SrInitial[2], Number)
-            # push!(df, [1e-10, 1e-18, SrInitial[1], SrInitial[2]])        
+        # push!(df, [1e-10, 1e-18, SrInitial[1], SrInitial[2]])
     end
     if SEtype == "abs"
         if dfCols == 5
-            β₀, β₀SE, β₁, β₁SE, χ²ᵣ, pval, σᵦ₁ᵦ₀, nX = york(df[:, 1], df[:, 2] ./ SElevel, df[:, 3], df[:, 4] ./ SElevel,
-                df[:, 5])
+            β₀, β₀SE, β₁, β₁SE, χ²ᵣ, pval, σᵦ₁ᵦ₀, nX = york(
+                df[:, 1], df[:, 2] ./ SElevel, df[:, 3], df[:, 4] ./ SElevel, df[:, 5]
+            )
         elseif dfCols == 4
-            β₀, β₀SE, β₁, β₁SE, χ²ᵣ, pval, σᵦ₁ᵦ₀, nX = york(df[:, 1], df[:, 2] ./ SElevel, df[:, 3], df[:, 4] ./ SElevel)
+            β₀, β₀SE, β₁, β₁SE, χ²ᵣ, pval, σᵦ₁ᵦ₀, nX = york(
+                df[:, 1], df[:, 2] ./ SElevel, df[:, 3], df[:, 4] ./ SElevel
+            )
         else
-           throw(ArgumentError("Column width is not equal to 4 or 5. Some data is missing."))
+            throw(ArgumentError("Column width is not equal to 4 or 5. Some data is missing."))
         end
     elseif SEtype == "rel"
         if dfCols == 5
-            β₀, β₀SE, β₁, β₁SE, χ²ᵣ, pval, σᵦ₁ᵦ₀, nX = york(df[:, 1], (df[:, 2] .* df[:, 1]) ./ SElevel, df[:, 3],
-                (df[:, 4] .* df[:, 3]) ./ SElevel, df[:, 5])
+            β₀, β₀SE, β₁, β₁SE, χ²ᵣ, pval, σᵦ₁ᵦ₀, nX = york(
+                df[:, 1], (df[:, 2] .* df[:, 1]) ./ SElevel, df[:, 3], (df[:, 4] .* df[:, 3]) ./ SElevel, df[:, 5]
+            )
         elseif dfCols == 4
-            β₀, β₀SE, β₁, β₁SE, χ²ᵣ, pval, σᵦ₁ᵦ₀, nX = york(df[:, 1], (df[:, 2] .* df[:, 1]) ./ SElevel, df[:, 3],
-                (df[:, 4] .* df[:, 3]) ./ SElevel)
+            β₀, β₀SE, β₁, β₁SE, χ²ᵣ, pval, σᵦ₁ᵦ₀, nX = york(
+                df[:, 1], (df[:, 2] .* df[:, 1]) ./ SElevel, df[:, 3], (df[:, 4] .* df[:, 3]) ./ SElevel
+            )
         else
-           throw(ArgumentError("Column width is not equal to 4 or 5. Some data is missing."))
+            throw(ArgumentError("Column width is not equal to 4 or 5. Some data is missing."))
         end
     end
     if SrInitial !== nothing
@@ -108,7 +115,9 @@ end
 function deming(X, Y)
     nX::Int = length(X)
     if nX !== length(Y)
-       throw(ArgumentError("Lengths of data vectors are not equal. Please ensure all data (X & Y) are of equal length."))
+        throw(
+            ArgumentError("Lengths of data vectors are not equal. Please ensure all data (X & Y) are of equal length.")
+        )
     else
         λ::Float64 = (var(Y)) / (var(X))
         X̄::Float64 = mean(X)
@@ -123,11 +132,11 @@ function deming(X, Y)
 end
 
 #jackknife deming
-function jackknife(df::DataFrame, method::String="deming")
+function jackknife(df::DataFrame, method::String = "deming")
     nX::Int = nrow(df)
     if method == "deming"
         jackknifed_estimates::Matrix = zeros(Float64, nX, 2)
-        @simd for j_position ∈ 1:nX
+        @simd for j_position in 1:nX
             β₀est::Float64, β₁est::Float64 = deming(df[Not(j_position), 1], df[Not(j_position), 2])
             jackknifed_estimates[j_position, 1] = β₀est
             jackknifed_estimates[j_position, 2] = β₁est
@@ -138,12 +147,12 @@ function jackknife(df::DataFrame, method::String="deming")
     end
 end
 
-function york(X::Vector{Float64}, sX::Vector{Float64}, Y::Vector{Float64}, sY::Vector{Float64}, ρXY=nothing)
+function york(X::Vector{Float64}, sX::Vector{Float64}, Y::Vector{Float64}, sY::Vector{Float64}, ρXY = nothing)
     nX::Int = length(X)
     if ρXY === nothing
         ρXY::Vector{Float64} = zeros(nX)
     elseif length(ρXY) !== nX
-        ρXY = push!(zeros(nX-length(ρXY)))
+        ρXY = push!(zeros(nX - length(ρXY)))
     end
     β₀::Float64, β₁::Float64 = coeffs(Polynomials.fit(X, Y, 1))
     βₑ::Float64 = β₁
