44f49e1afe7f0e5d89b61f4411062aaf91d9806a
Jarred C Lloyd <jarredclloyd@gmail.com>
2023-07-03 15:09:32 +0930

    doc formatting

--- src/ICP_MS.jl
+++ src/ICP_MS.jl
@@ -9,7 +9,7 @@ export load_downhole, plot_dh_scatter
 
 Load and prepare data from CSV agilent CSV files to assess downhole fractionation.
 
-Load all relevant files in the host_directory using glob, concatenates the data into one table, calculates ratio between
+Load all relevant files in the `host_directory` using glob, concatenates the data into one table, calculates ratio between
 `cps_column1` and `cps_column2`, the median-normalised ratio, and sorts the table by time.
 
 # Arguments
@@ -20,14 +20,22 @@ Load all relevant files in the host_directory using glob, concatenates the data 
 
 ## Argument Notes
 - `host_directory` will need to be wrapped by `raw` on Windows operating systems.
--  `sample` only needs to be a unique portion of the sample name. E.g. NIST610 will just load `NIST610*.csv` files, whereas NIST will load any file where the name contains `NIST*.csv`).
-- `cps_columnX` should be specified as `element` then `isotopic mass` (e.g., `Rb85`). It matches using regular expression and normalises the header names. If duplicate isotope values are present (due to mass shift checks, e.g. K39 -> 39, K39 -> 58) specify the desired mass by replacing ` -> ` with `_` (e.g. `K39 -> 39` to `K39_39`).
+- `sample` only needs to be a unique portion of the sample name. E.g. NIST610 will just load `NIST610*.csv` files,
+    whereas NIST will load any file where the name contains `NIST*.csv`).
+- `cps_columnX` should be specified as `element` then `isotopic mass` (e.g., `Rb85`). It matches using regular
+    expression and normalises the header names. If duplicate isotope values are present (due to mass shift checks, e.g.
+    K39 -> 39, K39 -> 58) specify the desired mass by replacing ` -> ` with `_` (e.g. `K39 -> 39` to `K39_39`).
 
 # Keywords
-- `firstrow::Int`: An `Int` representing the first data row. Default value is `5`. This can be used to remove the rows prior to `stabletime` if set to an appropriate value (e.g, ~110 for a 30 second gas blank).
-- `stabletime::Real`: The time (in seconds) where signal counts are stabilised. Default value is `32` (i.e. 32 seconds). This parameter is used to filter the rows when calculating the median counts per second ratio.
-- `signalend::Real`: The time (in seconds) where the data should be truncated. Default value is `Inf` (will use entire signal beyond `stabletime`). It is recommended to set this to a value appropriate to your data (i.e. total signal time minus ~5 [e.g. 65]) to avoid some artefacts  that may occur near the end of an ablation.
-- `trim::Bool`: Whether to trim the data to only contain values between `firstrow` and `signalend`. Default value is `false`. Setting to `true` it will delete all data from the first record past `signalend` to the last record.
+- `firstrow::Int`: An `Int` representing the first data row. Default value is `5`. This can be used to remove the rows
+    prior to `stabletime` if set to an appropriate value (e.g, ~110 for a 30 second gas blank).
+- `stabletime::Real`: The time (in seconds) where signal counts are stabilised. Default value is `32` (i.e. 32 seconds).
+    This parameter is used to filter the rows when calculating the median counts per second ratio.
+- `signalend::Real`: The time (in seconds) where the data should be truncated. Default value is `Inf` (will use entire
+    signal beyond `stabletime`). It is recommended to set this to a value appropriate to your data (i.e. total signal
+    time minus ~5 [e.g. 65]) to avoid some artefacts  that may occur near the end of an ablation.
+- `trim::Bool`: Whether to trim the data to only contain values between `firstrow` and `signalend`. Default value is
+    `false`. Setting to `true` it will delete all data from the first record past `signalend` to the last record.
 
 # Example
 ```
