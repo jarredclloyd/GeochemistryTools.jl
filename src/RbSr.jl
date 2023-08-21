721d41465ba8943db2304451078904e06e2c9db9
Jarred C Lloyd <jarredclloyd@gmail.com>
2023-08-21 10:03:28 +0930

    Formatting updates

--- src/RbSr.jl
+++ src/RbSr.jl
@@ -10,9 +10,19 @@ export ageRbSr, RbSrAgeNorm, RbSrAgeInv, confidenceInterval, inflateCI, inflateS
 
     calculates ages and uncertainties of RbSr data.
 
+    # Arguments
+
 """
-function ageRbSr(β₁, β₀=0.0, β₁SE=0.0, β₀SE=0.0, σᵦ₁ᵦ₀=0.0; 
-    inverse = false, SElevel_in = 2, SElevel_out = 2)
+function ageRbSr(
+    β₁::AbstractFloat,
+    β₀::AbstractFloat = 0.0,
+    β₁SE::AbstractFloat = 0.0,
+    β₀SE::AbstractFloat = 0.0,
+    σᵦ₁ᵦ₀::AbstractFloat = 0.0;
+    inverse::Bool = false,
+    SElevel_in::Int = 2,
+    SElevel_out::Int = 2,
+)
     if inverse == false
         if .==(β₁, 0.0) == true
             throw(ArgumentError("Non-zero values ∈ ℝ are required for slope (β₁) to calculate ages for isochron data."))
@@ -28,14 +38,17 @@ function ageRbSr(β₁, β₀=0.0, β₁SE=0.0, β₀SE=0.0, σᵦ₁ᵦ₀=0.0;
                 age[i], ageSE[i] = RbSrAgeNorm(β₁[i], β₁SE[i] / SElevel_in)
                 ageSE[i] = ageSE[i] * SElevel_out
             end
-        else 
+        else
             age, ageSE = RbSrAgeNorm(β₁, β₁SE / SElevel_in)
             ageSE = ageSE * SElevel_out
         end
     elseif inverse == true
         if .==(β₀, 0.0) == true || .==(β₁, 0.0) == true
-            throw(ArgumentError("Non-zero values ∈ ℝ are required for both the intercept (β₀) and the slope (β₁) to calculate ages for 
-            inverse isochron data."))
+            throw(
+                ArgumentError(
+                    "Non-zero values ∈ ℝ are required for both the intercept (β₀) and the slope (β₁) to calculate ages for inverse isochron data.",
+                ),
+            )
         end
         if isa(β₁, Array) == true && isa(β₀, Array) == true && length(β₁) == length(β₀)
             nslopes = length(β₁)
@@ -54,42 +67,51 @@ function ageRbSr(β₁, β₀=0.0, β₁SE=0.0, β₀SE=0.0, σᵦ₁ᵦ₀=0.0;
                 age[i], ageSE[i] = RbSrAgeInv(β₀[i], β₁[i], β₀SE[i] / SElevel_in, β₁SE[i] / SElevel_in, σᵦ₁ᵦ₀[i])
                 ageSE[i] = ageSE[i] * SElevel_out
             end
-        else 
+        else
             age, ageSE = RbSrAgeInv(β₀, β₁, β₀SE / SElevel_in, β₁SE / SElevel_in)
             ageSE = ageSE * SElevel_out
         end
     end
     return age, ageSE
-end 
+end
 
-function RbSrAgeNorm(β₁::Real, β₁SE::Real=0.0)
+function RbSrAgeNorm(β₁::AbstractFloat, β₁SE::AbstractFloat = 0.0)
     if .==(β₁, 0.0) == true
-         throw(ArgumentError("Non-zero values ∈ ℝ are required for slope (β₁) to calculate ages for isochron data."))
+        throw(ArgumentError("Non-zero values ∈ ℝ are required for slope (β₁) to calculate ages for isochron data."))
     end
     date = log(β₁ + 1) / λRb87
     dateSE = abs(log(β₁SE + 1) / λRb87)
     return date, dateSE
 end
 
-function RbSrAgeInv(β₀::Real, β₁::Real, β₀SE::Real=0.0, β₁SE::Real=0.0, σᵦ₁ᵦ₀::Real=0.0)
+function RbSrAgeInv(
+    β₀::AbstractFloat,
+    β₁::AbstractFloat,
+    β₀SE::AbstractFloat = 0.0,
+    β₁SE::AbstractFloat = 0.0,
+    σᵦ₁ᵦ₀::AbstractFloat = 0.0,
+)
     if .==(β₀, 0.0) == true || .==(β₁, 0.0) == true
-             throw(ArgumentError("Non-zero values ∈ ℝ are required for both the intercept (β₀) and the slope (β₁) to calculate ages for 
-            inverse isochron data."))
+        throw(
+            ArgumentError(
+                "Non-zero values ∈ ℝ are required for both the intercept (β₀) and the slope (β₁) to calculate ages for inverse isochron data.",
+            ),
+        )
     end
     ratio = inv(-β₀ / β₁)
     date = log(ratio + 1) / λRb87
     ratioSE = ratio * sqrt((β₀SE / β₀)^2 + (β₁SE / β₁)^2 - 2 * σᵦ₁ᵦ₀ / (β₀ * β₁))
-    dateSE = abs(log(ratioSE + (β₀ ^2 * β₀SE ^2 + β₁ ^2 * β₁SE ^2 + 2 * β₀ * β₁ * σᵦ₁ᵦ₀) + 1) / λRb87)
+    dateSE = abs(log(ratioSE + (β₀^2 * β₀SE^2 + β₁^2 * β₁SE^2 + 2 * β₀ * β₁ * σᵦ₁ᵦ₀) + 1) / λRb87)
     return date, dateSE
 end
 
-function confidenceInterval(SE, n; SElevel=1, CIlevel = 0.95)
-    (SE / SElevel) * cquantile(TDist(n - 2), (1 - CIlevel) / 2)
+function confidenceInterval(SE, n; SElevel = 1, CIlevel = 0.95)
+    return (SE / SElevel) * cquantile(TDist(n - 2), (1 - CIlevel) / 2)
 end
 
 function inflateSE(SE, χ²ᵣ)
-    SE * sqrt(χ²ᵣ)
+    return SE * sqrt(χ²ᵣ)
 end
 function inflateCI(SE, χ²ᵣ, n; SElevel = 1, CIlevel = 0.95)
-    (SE / SElevel) * cquantile(TDist(n - 2), (1 - CIlevel) / 2 ) * sqrt(χ²ᵣ)
+    return (SE / SElevel) * cquantile(TDist(n - 2), (1 - CIlevel) / 2) * sqrt(χ²ᵣ)
 end
