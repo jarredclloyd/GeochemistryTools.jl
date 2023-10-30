b2be46e5f482dfade5347cec204483adfbbabea5
Jarred C Lloyd <jarredclloyd@gmail.com>
2023-10-30 17:03:23 +1030

    Update beta_minus_decay_systems.jl

--- src/Geochronology/beta_minus_decay_systems.jl
+++ src/Geochronology/beta_minus_decay_systems.jl
@@ -2,11 +2,11 @@
 This file contains various decay system equations for calculation of ratios and ages (Rb—Sr).
 =#
 
-export ageRbSr, RbSrAgeNorm, RbSrAgeInv, confidenceInterval, inflateCI, inflateSE
+export ageRbSr, RbSrAgeNorm, RbSrAgeInv, confidenceInterval, inflateCI, inflate_se
 
 #Rb-Sr
 """
-    ageRbSr(β₁, [β₀=0.0, β₁SE=0.0, β₀SE=0.0, σᵦ₁ᵦ₀=0.0; inverse = false, se_level_in = 2, se_level_out = 2])
+    ageRbSr(β₁, [β₀=0.0, β₁_se=0.0, β₀_se=0.0, σᵦ₁ᵦ₀=0.0; inverse = false, se_level_in = 2, se_level_out = 2])
 
     calculates ages and uncertainties of RbSr data.
 
@@ -15,8 +15,8 @@ export ageRbSr, RbSrAgeNorm, RbSrAgeInv, confidenceInterval, inflateCI, inflateS
 function ageRbSr(
     β₁::AbstractFloat,
     β₀::AbstractFloat = 0.0,
-    β₁SE::AbstractFloat = 0.0,
-    β₀SE::AbstractFloat = 0.0,
+    β₁_se::AbstractFloat = 0.0,
+    β₀_se::AbstractFloat = 0.0,
     σᵦ₁ᵦ₀::AbstractFloat = 0.0;
     inverse::Bool = false,
     se_level_in::Int = 1,
@@ -34,15 +34,15 @@ function ageRbSr(
             nslopes = length(β₁)
             age = zeros(nslopes)
             age_se = zeros(nslopes)
-            if isa(β₁SE, Array) == false
-                β₁SE = zeros(nslopes)
+            if isa(β₁_se, Array) == false
+                β₁_se = zeros(nslopes)
             end
             @simd for i = 1:nslopes
-                age[i], age_se[i] = RbSrAgeNorm(β₁[i], β₁SE[i] / se_level_in)
+                age[i], age_se[i] = RbSrAgeNorm(β₁[i], β₁_se[i] / se_level_in)
                 age_se[i] = age_se[i] * se_level_out
             end
         else
-            age, age_se = RbSrAgeNorm(β₁, β₁SE / se_level_in)
+            age, age_se = RbSrAgeNorm(β₁, β₁_se / se_level_in)
             age_se = age_se * se_level_out
         end
     elseif inverse == true
@@ -57,11 +57,11 @@ function ageRbSr(
             nslopes = length(β₁)
             age = zeros(nslopes)
             age_se = zeros(nslopes)
-            if isa(β₀SE, Array) == false
-                β₀SE = zeros(nslopes)
+            if isa(β₀_se, Array) == false
+                β₀_se = zeros(nslopes)
             end
-            if isa(β₁SE, Array) == false
-                β₁SE = zeros(nslopes)
+            if isa(β₁_se, Array) == false
+                β₁_se = zeros(nslopes)
             end
             if isa(σᵦ₁ᵦ₀, Array) == false
                 σᵦ₁ᵦ₀ = zeros(nslopes)
@@ -70,14 +70,14 @@ function ageRbSr(
                 age[i], age_se[i] = RbSrAgeInv(
                     β₀[i],
                     β₁[i],
-                    β₀SE[i] / se_level_in,
-                    β₁SE[i] / se_level_in,
+                    β₀_se[i] / se_level_in,
+                    β₁_se[i] / se_level_in,
                     σᵦ₁ᵦ₀[i],
                 )
                 age_se[i] = age_se[i] * se_level_out
             end
         else
-            age, age_se = RbSrAgeInv(β₀, β₁, β₀SE / se_level_in, β₁SE / se_level_in)
+            age, age_se = RbSrAgeInv(β₀, β₁, β₀_se / se_level_in, β₁_se / se_level_in)
             age_se = age_se * se_level_out
         end
     end
@@ -95,7 +95,7 @@ function ageRbSr(fit::ErrorsInVariablesRegression; inverse = true, se_level_out:
     return age, age_se
 end
 
-function RbSrAgeNorm(β₁::AbstractFloat, β₁SE::AbstractFloat = 0.0)
+function RbSrAgeNorm(β₁::AbstractFloat, β₁_se::AbstractFloat = 0.0)
     if .==(β₁, 0.0) == true
         throw(
             ArgumentError(
@@ -104,15 +104,15 @@ function RbSrAgeNorm(β₁::AbstractFloat, β₁SE::AbstractFloat = 0.0)
         )
     end
     date = log(β₁ + 1) / λRb87
-    dateSE = abs(log(β₁SE + 1) / λRb87)
-    return date, dateSE
+    date_se = abs(log(β₁_se + 1) / λRb87)
+    return date, date_se
 end
 
 function RbSrAgeInv(
     β₀::AbstractFloat,
     β₁::AbstractFloat,
-    β₀SE::AbstractFloat = 0.0,
-    β₁SE::AbstractFloat = 0.0,
+    β₀_se::AbstractFloat = 0.0,
+    β₁_se::AbstractFloat = 0.0,
     σᵦ₁ᵦ₀::AbstractFloat = 0.0,
 )
     if .==(β₀, 0.0) == true || .==(β₁, 0.0) == true
@@ -124,20 +124,20 @@ function RbSrAgeInv(
     end
     ratio = inv(-β₀ / β₁)
     date = log(ratio + 1) / λRb87
-    ratioSE = ratio * sqrt((β₀SE / β₀)^2 + (β₁SE / β₁)^2 - 2 * σᵦ₁ᵦ₀ / (β₀ * β₁))
-    dateSE = abs(
-        log(ratioSE + (β₀^2 * β₀SE^2 + β₁^2 * β₁SE^2 + 2 * β₀ * β₁ * σᵦ₁ᵦ₀) + 1) / λRb87,
+    ratio_se = ratio * sqrt((β₀_se / β₀)^2 + (β₁_se / β₁)^2 - 2 * σᵦ₁ᵦ₀ / (β₀ * β₁))
+    date_se = abs(
+        log(ratio_se + (β₀^2 * β₀_se^2 + β₁^2 * β₁_se^2 + 2 * β₀ * β₁ * σᵦ₁ᵦ₀) + 1) / λRb87,
     )
-    return date, dateSE
+    return date, date_se
 end
 
-function confidenceInterval(SE, n; se_level = 1, CIlevel = 0.95)
-    return (SE / se_level) * cquantile(TDist(n - 2), (1 - CIlevel) / 2)
+function confidenceInterval(se, n; se_level = 1, ci_level = 0.95)
+    return (se / se_level) * cquantile(TDist(n - 2), (1 - ci_level) / 2)
 end
 
-function inflateSE(SE, χ²ᵣ)
-    return SE * sqrt(χ²ᵣ)
+function inflate_se(se, χ²ᵣ)
+    return se * sqrt(χ²ᵣ)
 end
-function inflateCI(SE, χ²ᵣ, n; se_level = 1, CIlevel = 0.95)
-    return (SE / se_level) * cquantile(TDist(n - 2), (1 - CIlevel) / 2) * sqrt(χ²ᵣ)
+function inflateCI(se, χ²ᵣ, n; se_level = 1, ci_level = 0.95)
+    return (se / se_level) * cquantile(TDist(n - 2), (1 - ci_level) / 2) * sqrt(χ²ᵣ)
 end
