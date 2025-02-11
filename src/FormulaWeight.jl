cba47b04571733d6dab6b8af13dc3eecefcbfed3
Jarred C Lloyd <74882990+jarredclloyd@users.noreply.github.com>
2025-02-11 16:21:58 +1030

    rename to molecular_mass

--- src/FormulaWeight.jl
+++ src/FormulaWeight.jl
@@ -2,40 +2,52 @@
 This file contains various decay system equations for calculation of ratios and ages (U-Pb).
 =#
 
-export molecularwt
+export molecular_mass
 
 """
-    molecularwt(formula)
+    molecular_mass(formula; verbose::Bool=true)
 
-Calculates molar weight of input formula. Multiple formulas can be passed as an array.
+Calculates molar weight of input formula.
+
+Multiple formulas can be passed as vector of strings.
 Use only standard characters and do not superscript mole numbers. Additional ions after a • should be added in
 parentheses if there are multiple moles of it, e.g., •2(H20) should be entered as (H20)2.
 
+# Keywords
+    `-verbose::Bool=true` set to false to suppress detailed output to REPL.
+
 ```julia-repl
-julia> molecularwt("Na0.84Ca0.64K0.44Ba0.02Sr0.01Ca0.64Mn0.05Zn0.01Nb2.7Ti1.28Fe0.07Si7.97Al0.06O24.09OH1.23O2.75(H2O)6.92")
+julia> molecular_mass("Na0.84Ca0.64K0.44Ba0.02Sr0.01Ca0.64Mn0.05Zn0.01Nb2.7Ti1.28Fe0.07Si7.97Al0.06O24.09OH1.23O2.75(H2O)6.92")
 Molecular weight of input formula {Na0.84Ca0.64K0.44Ba0.02Sr0.01Ca0.64Mn0.05Zn0.01Nb2.7Ti1.28Fe0.07Si7.97Al0.06O24.09OH1.23O2.75(H2O)6.92} is:
 1207.6534935612 gmol⁻¹.
 
-julia> molecularwt(["Al2O3", "K2O"])
+julia> molecular_mass(["Al2O3", "K2O"])
 Molecular weight of input formula {K2O} is:
 94.196 gmol⁻¹.
 Molecular weight of input formula {Al2O3} is:
 101.9612772 gmol⁻¹.
 ```
 """
-function molecularwt(formula)
+function molecular_mass(formula::Union{String, Vector{T} where T <: AbstractString};
+    verbose::Bool=true)
     if isa(formula, Array)
         weight = zeros(size(formula))
         nformulas = length(formula)
-        @threads for i in 1:nformulas
+        @simd for i in 1:nformulas
             weight[i] = computemass(formula[i])
-            printstyled("Molecular weight of input formula {" * string(formula[i]) * "} is:\n" * string(weight[i]) *
+            if print == true
+                printstyled("Molecular weight of input formula {" * string(formula[i]) * "} is:\n" * string(weight[i]) *
                         " gmol⁻¹.\n", bold=true)
+            end
+            return weight
         end
     else
         weight = computemass(formula)
+        if print == true
         printstyled("Molecular weight of input formula {" * string(formula) * "} is:\n" * string(weight) *
                     " gmol⁻¹.", bold=true)
+        end
+        return weight
     end
 end
 
@@ -65,21 +77,21 @@ function computeionmass(formula, left_index)
             right_index = findnext(')', formula, left_index)
             parentheses_end = right_index
             if right_index - left_index == 1
-                mass = element_symbol_to_mass[formula[left_index]]
+                mass = atomic_mass[formula[left_index]]
                 left_index = left_index + 2
             elseif right_index - left_index == 2    #e.g., (OH) (Mg) (B2)
                 right_index = right_index - 1
                 if formula[right_index] == uppercase(formula[right_index]) && isdigit(formula[right_index]) == false
-                    ion_mass = element_symbol_to_mass[formula[left_index]]
-                    ion_mass = ion_mass + element_symbol_to_mass[formula[right_index]]
+                    ion_mass = atomic_mass[formula[left_index]]
+                    ion_mass = ion_mass + atomic_mass[formula[right_index]]
                     left_index = left_index + 3
                 elseif formula[right_index] == uppercase(formula[right_index]) && isdigit(formula[right_index]) == true
-                    ion_mass = element_symbol_to_mass[formula[left_index]]
+                    ion_mass = atomic_mass[formula[left_index]]
                     n_moles = parse(Int, formula[right_index])
                     ion_mass = n_moles * ion_mass
                     left_index = left_index + 3
                 else
-                    ion_mass = element_symbol_to_mass[formula[left_index:right_index]]
+                    ion_mass = atomic_mass[formula[left_index:right_index]]
                     left_index = left_index + 3
                 end
                 n_moles, position_index = findnmoles(formula, left_index - 1)
@@ -89,27 +101,27 @@ function computeionmass(formula, left_index)
                 mass = 0   #reset mass before cumulative calculation through parenthesised ions
                 while right_index ≤ parentheses_end
                     if formula[right_index] == ')' && isdigit(formula[left_index]) == false   #e.g., H)
-                        ion_mass = element_symbol_to_mass[formula[left_index]]
+                        ion_mass = atomic_mass[formula[left_index]]
                         left_index = right_index + 1
                         right_index = left_index + 1
                         position_index = left_index
                     elseif isdigit(formula[right_index]) == true && formula[left_index] ==
                                                                    uppercase(formula[left_index])   #e.g., O10, O3
-                        ion_mass = element_symbol_to_mass[formula[left_index]]
+                        ion_mass = atomic_mass[formula[left_index]]
                         n_moles, position_index = findnmoles(formula, right_index)
                         ion_mass = n_moles * ion_mass
                         left_index = position_index
                         right_index = left_index + 1
                     elseif isdigit(formula[right_index]) == false && formula[right_index] ==
                                                                     lowercase(formula[right_index])  #e.g., Mg
-                        ion_mass = element_symbol_to_mass[formula[left_index:right_index]]
+                        ion_mass = atomic_mass[formula[left_index:right_index]]
                         n_moles, position_index = findnmoles(formula, right_index + 1)
                         ion_mass = n_moles * ion_mass
                         left_index = position_index
                         right_index = left_index + 1
                     elseif isdigit(formula[right_index]) == false && formula[right_index] ==
                                                                     uppercase(formula[right_index]) #e.g., O in OH
-                        ion_mass = element_symbol_to_mass[formula[left_index]]
+                        ion_mass = atomic_mass[formula[left_index]]
                         left_index = right_index
                         right_index = left_index + 1
                         position_index = left_index
@@ -131,20 +143,20 @@ function computeionmass(formula, left_index)
         left_index = position_index
     elseif right_index ≤ formula_length
         if isdigit(formula[right_index]) == true && formula[left_index] == uppercase(formula[left_index])    #e.g., B2
-            ion_mass = element_symbol_to_mass[formula[left_index]]
+            ion_mass = atomic_mass[formula[left_index]]
             n_moles, position_index = findnmoles(formula, right_index)
             mass = ion_mass * n_moles
         elseif isdigit(formula[right_index]) == false && formula[right_index] == lowercase(formula[right_index])     #e.g., Mg
-            ion_mass = element_symbol_to_mass[formula[left_index:right_index]]
+            ion_mass = atomic_mass[formula[left_index:right_index]]
             n_moles, position_index = findnmoles(formula, right_index + 1)
             mass = ion_mass * n_moles
         elseif formula[right_index] == uppercase(formula[right_index])      #e.g., O in MgOH
-            ion_mass = element_symbol_to_mass[formula[left_index]]
+            ion_mass = atomic_mass[formula[left_index]]
             mass = ion_mass
             position_index = right_index
         end
     elseif right_index > formula_length && formula[left_index] == uppercase(formula[left_index])   #e.g., O  in MgO
-        ion_mass = element_symbol_to_mass[formula[left_index]]
+        ion_mass = atomic_mass[formula[left_index]]
         mass = ion_mass
         position_index = right_index
     end
