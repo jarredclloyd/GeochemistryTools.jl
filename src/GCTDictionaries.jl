c9266dc05bd1f545e0b67664c2af4092b069251e
Jarred C Lloyd <74882990+jarredclloyd@users.noreply.github.com>
2025-02-12 16:52:24 +1030

    Update GCTDictionaries.jl

--- src/GCTDictionaries.jl
+++ src/GCTDictionaries.jl
@@ -3,7 +3,6 @@ This jl file contains dictionaries and other reference information for that are 
 =#
 
 export avogadro,
-    atomic_mass,
     molecular_weight_oxide,
     dict_sr87_sr86i, cn_eight_IR,
     ci_chondrite_PO2016,
@@ -28,7 +27,6 @@ const avogadro::AbstractFloat = 6.02214076 * 10^23
 const planck::AbstractFloat = 6.62607015e-34 #JHz⁻¹
 const speedoflight::AbstractFloat = 299792458 #ms⁻¹
 const boltzmann::AbstractFloat = 1.380649e-23 #JK⁻¹
-const (d::Dict)(key) = d[key]
 
 #Constant Declarations: decay constants
 const λK40::AbstractFloat, SEλK40::AbstractFloat = 0.00055305, 0.00000132
@@ -48,43 +46,8 @@ const λU238::AbstractFloat, SEλU238::AbstractFloat = 0.000155125, 0.000000083
 const U238U235::AbstractFloat, SE238U235::AbstractFloat = 137.818, 0.0225
 
 #Element properties (mass, ionic radii, charges, symbol, name)
-element_symbol_to_name = Dict()
 
-"""
-    atomic_mass(x)
-Return the IUPAC aggregate atomic mass (gmol⁻¹) for a given element.
 
-Single character elements must be entered as a character using ' '.
-Double character elements must be entered as a string " ".
-
-#Examples
-```julia-repl
-julia > atomic_mass('H')
-1.00794
-julia> atomic_mass("He")
-4.002602
-```
-"""
-const atomic_mass::AbstractDict = Dict{Any, AbstractFloat}(
-    [('H', 1.00794), ("He", 4.002602), ("Li", 6.941), ("Be", 9.012182), ('B', 10.811),
-    ('C', 12.0107), ('N', 14.0067), ('O', 15.9994), ('F', 18.9984032), ("Ne", 20.1797),
-    ("Na", 22.98976928), ("Mg", 24.305), ("Al", 26.9815386), ("Si", 28.0855),
-    ('P', 30.973762), ('S', 32.065), ("Cl", 35.453), ("Ar", 39.948), ('K', 39.0983),
-    ("Ca", 40.078), ("Sc", 44.955912), ("Ti", 47.867), ('V', 50.9415), ("Cr", 51.9961),
-    ("Mn", 54.938045), ("Fe", 55.845), ("Co", 58.933195), ("Ni", 58.6934), ("Cu", 63.546),
-    ("Zn", 65.409), ("Ga", 69.723), ("Ge", 72.64), ("As", 74.9216), ("Se", 78.96),
-    ("Br", 79.904), ("Kr", 83.798), ("Rb", 85.4678), ("Sr", 87.62), ('Y', 88.90585),
-    ("Zr", 91.224), ("Nb", 92.90638), ("Mo", 95.94), ("Ru", 101.07), ("Rh", 102.9055),
-    ("Pd", 106.42), ("Ag", 107.8682), ("Cd", 112.411), ("In", 114.818), ("Sn", 118.71),
-    ("Sb", 121.76), ("Te", 127.6), ('I', 126.90447), ("Xe", 131.293), ("Cs", 132.9054519),
-    ("Ba", 137.327), ("La", 138.90547), ("Ce", 140.116), ("Pr", 140.90765), ("Nd", 144.242),
-    ("Sm", 150.36), ("Eu", 151.964), ("Gd", 157.25), ("Tb", 158.92535), ("Dy", 162.5),
-    ("Ho", 164.93032), ("Er", 167.259), ("Tm", 168.93421), ("Yb", 173.04), ("Lu", 174.967),
-    ("Hf", 178.49), ("Ta", 180.94788), ("W", 183.84), ("Re", 186.207), ("Os", 190.23),
-    ("Ir", 192.217), ("Pt", 195.084), ("Au", 196.966569), ("Hg", 200.59),
-    ("Tl", 204.3833), ("Pb", 207.2), ("Bi", 208.9804), ("Th", 232.03806), ("Pa", 231.03588),
-    ('U', 238.0289)]
-)
 
 """
     cn_eight_IR(x)
