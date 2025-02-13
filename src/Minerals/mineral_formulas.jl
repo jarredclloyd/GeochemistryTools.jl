10cdcf9d715d39543201a773ede500ecd1b30800
Jarred C Lloyd <jarredclloyd@gmail.com>
2025-02-13 22:14:58 +1030

    WIP

--- src/Minerals/mineral_formulas.jl
+++ src/Minerals/mineral_formulas.jl
@@ -10,97 +10,113 @@ Calculates mica formula based on input data and declared units.
 
 # Description
 Data is a data frame containing sample name and elemental data (either as element or oxide) in a consistent unit
-(either ppm OR wt%).
+(either ppm OR wt%O).
 
-Units is the units the data is in, a string of value "ppm" OR "wt%"
+Units is the units the data is in, a string of value "ppm" OR "wt%O"
 
 # Example
 ```julia-repl
 julia> formula_mica(df, "ppm")
 ```
 """
-function formula_mica(data::AbstractDataFrame, units::AbstractString; normalising_O::Int=11)
-    I_site = [:Na, :K, :Ca, :Rb, :Cs, :Ba]
-    M_site = [:Li, :Mg, :Ti, :V, :Cr, :Mn, :Zn, :Fe]
-    TM_site = [:Al]
-    T_site = [:Be, :B, :Si]
-    A_site = [:F, :Cl, :OH]
-    column_names = [:Na, :K, :Ca, :Rb, :Cs, :Ba, :Li, :Mg, :Ti, :V, :Cr, :Mn, :Zn, :Fe, :Al, :Be, :B, :Si, :F, :Cl, :OH]
+function formula_mica(data::AbstractDataFrame, units::AbstractString; normalising_O::Int=22)
+    I_site = [:NH4, :Na, :K, :Ca, :Rb, :Cs, :Ba]
+    M_site = [:Li, :Mg, :Ti, :V, :Cr, :MnO, :Mn2O3, :Zn, :FeO, :Fe2O3, :Al]
+    T_site = [:Fe2O3, :Al, :Be, :B, :Si]
+    A_site = [:F, :Cl, :OH, :S],
+    column_names = [:NH4, :Na, :K, :Ca, :Rb, :Cs, :Ba, :Li, :Mg, :Ti, :V, :Cr, :MnO, :Mn2O3, :Zn, :FeO, :Fe2O3, :Al, :Be, :B, :Si, :F, :Cl, :OH, :S]
     workingdata = _find_columns_mica(data)
     workingvector = collect(values(workingdata[1, Not(:Sample)]))
 
     if lowercase(units) == "ppm"
-        oxide_conversion = [
-            1.347955633, 1.204601258, 1.399196567, 1.093596434, 1.060187345, 1.1165004, 2.152665706, 1.658259617,
-            1.668477239, 1.628126104, 1.461545119, 1.291219193, 1.244707862, 1.28648939, 1.889426284, 2.775260203,
-            3.220027752, 2.139327043, 1.0, 1.0, 8.936011905
+        oxide_conversion_factors = [
+            cation_to_oxide(1; cation=:"NH4", cation_multiplicity = 2, oxide = "(NH4)2O"),
+            element_to_oxide(1; element=:Na, element_multiplicity=2, oxide = "Na2O").val,
+            element_to_oxide(1; element=:K, element_multiplicity=2, oxide = "K2O").val,
+            element_to_oxide(1; element=:Ca, element_multiplicity=1, oxide = "CaO").val,
+            element_to_oxide(1; element=:Rb, element_multiplicity=2, oxide = "Rb2O").val,
+            element_to_oxide(1; element=:Cs, element_multiplicity=2, oxide = "Cs2O").val,
+            element_to_oxide(1; element=:Ba, element_multiplicity= 1, oxide = "BaO").val,
+            element_to_oxide(1; element=:Li, element_multiplicity= 2, oxide = "Li2O").val,
+            element_to_oxide(1; element=:Mg, element_multiplicity= 1, oxide = "MgO").val,
+            element_to_oxide(1; element=:Ti, element_multiplicity= 1, oxide = "TiO2").val,
+            element_to_oxide(1; element=:V, element_multiplicity= 1, oxide = "VO2").val,
+            element_to_oxide(1; element=:Cr, element_multiplicity= 2, oxide = "Cr2O3").val,
+            element_to_oxide(1; element=:Mn, element_multiplicity= 1, oxide = "MnO").val,
+            element_to_oxide(1; element=:Mn, element_multiplicity= 2, oxide ="Mn2O3").val,
+            element_to_oxide(1; element=:Zn, element_multiplicity= 1, oxide = "ZnO").val,
+            element_to_oxide(1; element=:Fe, element_multiplicity=1, oxide = "FeO").val,
+            element_to_oxide(1; element=:Fe, element_multiplicity=2, oxide = "Fe2O3").val,
+            element_to_oxide(1; element=:Al, element_multiplicity= 2, oxide = "Al2O3").val,
+            element_to_oxide(1; element=:Be, element_multiplicity= 1, oxide = "BeO").val,
+            element_to_oxide(1; element=:B, element_multiplicity= 2, oxide = "B2O3").val,
+            element_to_oxide(1; element=:Si, element_multiplicity= 1, oxide = "SiO2").val,
+            element_to_oxide(1; element=:F, element_multiplicity= 1, oxide = "F").val,
+            element_to_oxide(1; element=:Cl, element_multiplicity= 1, oxide = "Cl").val,
+            1, #OH
+            element_to_oxide(1; element=:S, element_multiplicity= 1, oxide = "S").val
         ]
-        weight_percent = (workingvector ./ 1e4) .* oxide_conversion
-    elseif lowercase(units) == "wt%"
+        weight_percent = element_to_oxide.(workingvector ./ 1e4) .* oxide_conversion_factors
+    elseif lowercase(units) == "wt%O"
         weight_percent = workingvector
     end
 
     molecular_weights = [
-        molecular_mass("Na2O"),
-        molecular_mass("K2O"),
-        molecular_mass("CaO"),
-        molecular_mass("Rb2O"),
-        molecular_mass("Cs2O"),
-        molecular_mass("BaO"),
-        molecular_mass("Li2O"),
-        molecular_mass("MgO"),
-        molecular_mass("TiO2"),
-        molecular_mass("VO2"),
-        molecular_mass("Cr2O3"),
-        molecular_mass("MnO"),
-        molecular_mass("ZnO"),
-        molecular_mass("FeO"),
-        molecular_mass("Fe2O3"),
-        molecular_mass("AlO"),
-        molecular_mass("Al2O3"),
-        molecular_mass("BeO"),
-        molecular_mass("B2O3"),
-        molecular_mass("SiO2"),
-        atomic_mass('F'),
-        atomic_mass("Cl"),
-        molecular_mass("H2O")
+        molecular_mass("(NH4)2O"; verbose = false),
+        molecular_mass("Na2O"; verbose = false),
+        molecular_mass("K2O"; verbose = false),
+        molecular_mass("CaO"; verbose = false),
+        molecular_mass("Rb2O"; verbose = false),
+        molecular_mass("Cs2O"; verbose = false),
+        molecular_mass("BaO"; verbose = false),
+        molecular_mass("Li2O"; verbose = false),
+        molecular_mass("MgO"; verbose = false),
+        molecular_mass("TiO2"; verbose = false),
+        molecular_mass("VO2"; verbose = false),
+        molecular_mass("Cr2O3"; verbose = false),
+        molecular_mass("MnO"; verbose = false),
+        molecular_mass("Mn2O3"; verbose = false),
+        molecular_mass("ZnO"; verbose = false),
+        molecular_mass("FeO"; verbose = false),
+        molecular_mass("Fe2O3"; verbose = false),
+        molecular_mass("Al2O3"; verbose = false),
+        molecular_mass("BeO"; verbose = false),
+        molecular_mass("B2O3"; verbose = false),
+        molecular_mass("SiO2"; verbose = false),
+        get_atomicmass(:F).val,
+        get_atomicmass(:Cl).val,
+        molecular_mass("OH"; verbose = false),
+        get_atomicmass(:S).val
     ]
 
     moles_compound = weight_percent ./ molecular_weights
-    oxide_factors = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 1, 1, 1, 3, 1, 3, 2, 0.5, 0.5, 0.5]
-    moles_oxygen = moles_compound .* oxide_factors
+    n_oxygen = [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 1, 3, 1, 1, 3, 3, 1, 3, 2, 0.5, 0.5, 0.5, -1]
+    moles_oxygen = moles_compound .* n_oxygen
     oxygen_sum = sum(moles_oxygen)
     normalising_factor = normalising_O / oxygen_sum
-    moles_oxygen[19] = moles_oxygen[19] * 2
-    moles_oxygen[20] = moles_oxygen[20] * 2
     normalised_oxygen_moles = moles_oxygen .* normalising_factor
 
     # iterate OH
-    iterations = 0
-    H_initial = 2 - (normalised_oxygen_moles[19] + normalised_oxygen_moles[20])
-    println("Initial H: ", H_initial)
-    O_OH = (0.5 * H_initial) / normalising_factor
-    moles_oxygen[21] = O_OH
-    oxygen_sum = sum(moles_oxygen) - 0.5 * (moles_oxygen[19] + moles_oxygen[20])
-    normalising_factor = normalising_O / oxygen_sum
-    normalised_oxygen_moles = moles_oxygen .* normalising_factor
-
     ϵ = 1
-    while ϵ > 1e-4 && iterations <= 1000
-        H_iteration = 2 - (normalised_oxygen_moles[19] + normalised_oxygen_moles[20])
-        println("H iteration: ", H_iteration)
-        O_OH = (0.5 * H_iteration) / normalising_factor
-        moles_oxygen[21] = O_OH
-        oxygen_sum = sum(moles_oxygen) - 0.5 * (moles_oxygen[19]+moles_oxygen[20])
-        normalising_factor = normalising_O / oxygen_sum
-        normalised_oxygen_moles = moles_oxygen .* normalising_factor
-        ϵ = abs(H_iteration - H_initial)
-        println("Oxygen Sum: ", oxygen_sum, ", Normalising Factor: ", normalising_factor)  # Debugging statement
-        iterations = iterations + 1
-        H_initial = H_iteration
-    end
+    OH_initial = normalised_oxygen_moles[24]
 
-    element_oxy_ratio = [2, 2, 1, 2, 2, 1, 2, 1, 0.5, 0.5, 0.667, 1, 1, 1, 0.667, 1, 0.667, 0.5, 1, 1, 2]D
+    if OH_initial == 0
+        iteration = 1
+        OH_initial = 2
+        while ϵ > 1e-4 && iteration <= 1000
+            OH_iteration = OH_initial - (normalised_oxygen_moles[22] + normalised_oxygen_moles[23] + normalised_oxygen_moles[25])
+            println("H iteration: $OH_iteration")
+            moles_oxygen[24] = (0.5 * OH_iteration)
+            oxygen_sum = sum(moles_oxygen)
+            normalising_factor = normalising_O / oxygen_sum
+            normalised_oxygen_moles = moles_oxygen .* normalising_factor
+            iteration += 1
+            ϵ = abs(OH_initial - OH_iteration)
+            println("epsilon: $ϵ")
+            OH_initial = OH_iteration
+        end
+    end
+    element_oxy_ratio = [2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 0.667, 2, 0.667, 2, 2, 0.667, 0.667, 2, 0.667, 1, 2, 2, 2, 1]
     atoms_per_formula_unit = normalised_oxygen_moles .* element_oxy_ratio
     return round.(atoms_per_formula_unit,digits=3)
 end
@@ -111,6 +127,11 @@ function _find_columns_mica(data)
         workingdata.Sample = data[:, findfirst(lowercase.(names(data)) .== "sample")]
     else
         workingdata.Sample .= "sample"
+    end
+    if "NH4" in names(data)
+        workingdata.NH4 = data[:, :NH4]
+    else
+        workingdata.NH4 .= 0
     end
     if "Na" in names(data)
         workingdata.Na = data[:, :Na]
@@ -189,12 +210,18 @@ function _find_columns_mica(data)
     else
         workingdata.Cr .= 0
     end
-    if "Mn" in names(data)
-        workingdata.Mn = data[:, :Mn]
-    elseif "MnO" in names(data)
-        workingdata.Mn = data[:, :MnO]
+    if "MnO" in names(data)
+        workingdata.MnO = data[:, :MnO]
     else
-        workingdata.Mn .= 0
+        workingdata.MnO .= 0
+    end
+    if "Mn2O3" in names(data)
+        workingdata.Mn2O3 = data[:, :Mn2O3]
+    else
+        workingdata.Mn2O3 .= 0
+    end
+    if "Mn" in names(data)
+        workingdata.Mn2O3 .= 0
     end
     if "Zn" in names(data)
         workingdata.Zn = data[:, :Zn]
@@ -204,19 +231,17 @@ function _find_columns_mica(data)
         workingdata.Zn .= 0
     end
     if "FeO" in names(data)
-            workingdata.FeO = data[:, :FeO]
+        workingdata.FeO = data[:, :FeO]
     else
         workingdata.FeO = 0
     end
     if "Fe2O3" in names(data)
         workingdata.Fe2O3 = data[:, :Fe2O3]
+    else
+        workingdata.Fe2O3 = 0
     end
     if "Fe" in names(data)
-            workingdata.FeO = 0
-            workingdata.Fe2O3 = 0
-            workingdata.Fe = data[:, :Fe]
-    else
-        workingdata.Fe .= 0
+        workingdata.Fe2O3 = data[:, :Fe2O3]
     end
     if "Al" in names(data)
         workingdata.Al = data[:, :Al]
@@ -258,10 +283,13 @@ function _find_columns_mica(data)
     end
     if "OH" in names(data)
         workingdata.OH = data[:, :OH]
-    elseif "H2O" in names(data)
-        workingdata.OH = data[:, :H2O]
     else
         workingdata.OH .= 0
+    end
+    if "S" in names(data)
+        workingdata.S = data[:, :S]
+    else
+        workingdata.S .= 0
     end
     return workingdata
 end
