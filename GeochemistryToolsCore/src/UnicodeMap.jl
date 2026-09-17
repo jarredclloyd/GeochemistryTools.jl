7aeeaa65d7f0174e224194d396e37470115ee7f8
Jarred C Lloyd <jarred.lloyd@adelaide.edu.au>
2026-09-17 15:08:31 +0930

    add unicodemapping for fancy mineral printing

---
+++ GeochemistryToolsCore/src/UnicodeMap.jl
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: Copyright © 2026 Jarred C. Lloyd
+# SPDX-FileContributor: Jarred C. Lloyd <4o8pg2v9h@mozmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+export TO_UNICODE, FROM_UNICODE
+
+const TO_UNICODE = Dict(
+    "0" => "₀",
+    "1" => "₁",
+    "2" => "₂",
+    "3" => "₃",
+    "4" => "₄",
+    "5" => "₅",
+    "6" => "₆",
+    "7" => "₇",
+    "8" => "₈",
+    "9" => "₉"
+)
+
+const FROM_UNICODE = Dict(
+    "₀" => "0",
+    "₁" => "1",
+    "₂" => "2",
+    "₃" => "3",
+    "₄" => "4",
+    "₅" => "5",
+    "₆" => "6",
+    "₇" => "7",
+    "₈" => "8",
+    "₉" => "9"
+)
